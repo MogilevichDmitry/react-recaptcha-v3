3be038a7caeaa8d8e3b1b369c2b912431053666f
mogilevichdmitry <dmsmain@gmail.com>
2019-04-06 00:38:57 +0300

    Add id to scipt

--- src/loadReCaptcha.js
+++ src/loadReCaptcha.js
@@ -1,6 +1,7 @@
 const loadReCaptcha = siteKey => {
   const script = document.createElement('script')
 
+  script.id = 'recaptcha-v3-scipt'
   script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`
 
   document.body.appendChild(script)
